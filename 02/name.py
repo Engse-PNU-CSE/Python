48c3cb8f8277937d13ace4774dec200b4803a243
Kim Engse <error302@naver.com>
2024-02-24 00:22:43 +0900

    24.02.24 v1

---
+++ 02/name.py
@@ -0,0 +1,10 @@
+name = "ada loVElace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+firstName = "ada"
+lastName = "lovelace"
+fullname = f"{firstName} {lastName}"
+
+print(fullname.title())
