bd926e776249c8cbee3f9b201ca01d0c3662d6f1
Kim Engse <error302@naver.com>
2024-02-26 13:09:58 +0900

    24.02.26 v4

---
+++ 04/magicians.py
@@ -0,0 +1,7 @@
+magicians = ['alice', 'david', 'carolia']
+
+for magician in magicians: #확장형 for문
+    print(magician + ", that was a great trick")
+    print("I can't wait to see your next trick, " + magician.title())
+
+print("Thank you, everyone. That was a great magic show!")
