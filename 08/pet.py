fe804d9f5b450e6c68c02a4099bcc8d37a6cd714
Kim Engse <error302@naver.com>
2024-02-27 17:58:26 +0900

    24.02.27 v5

--- 08/pet.py
+++ 08/pet.py
@@ -4,3 +4,11 @@ def describe_pet(animal_type, pet_name):
     print(f"My {animal_type}'s name is {pet_name.title()}")
 
 describe_pet('hamster', 'engse')
+
+def describe_pet2(animal_type, pet_name = 'hong'):
+    """show pet info"""
+    print(f"I have a {animal_type}")
+    print(f"My {animal_type}'s name is {pet_name.title()}")
+
+describe_pet2('hamster')
+describe_pet2('ham', 'kim')
