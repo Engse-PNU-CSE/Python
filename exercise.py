44d7bc24a2ff968ee0caa746be581f5eb8a2a329
Kim Engse <error302@naver.com>
2024-02-29 17:18:56 +0900

    24.02.29 v3

--- exercise.py
+++ exercise.py
@@ -1,20 +1,6 @@
-n = int(input())
-for i in range(n):
-    coin = [0, 0, 0, 0]
-    money = int(input())
-    while money > 0:
-        if(money >= 25):
-            coin[0]=int(money/25)
-            money -= int(money/25)*25
-        elif(money >= 10):
-            coin[1]=int(money/10)
-            money -= int(money/10)*10
-        elif(money >= 5):
-            coin[2]=int(money/5)
-            money -= int(money/5)*5
-        else:
-            coin[3]=money
-            money=0
-    for c in coin:
-        print(c, end=" ")
-    print()
+def cal(n):
+    if n == 0:
+        return 2
+    else:
+        return 2*cal(n-1)-1
+print(cal(int(input())) ** 2)
