5551e2fc77e40a049a6e0afea8bf8391ad3b4ddb
Kim Engse <error302@naver.com>
2024-03-04 17:57:10 +0900

    24.03.04 v5

--- 09/electricCar.py
+++ 09/electricCar.py
@@ -1,4 +1,66 @@
 class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometerReading = 0
+        self.gasTank = 0
+    
+    def getDescriptiveName(self):
+        longName = f"{self.year} {self.make} {self.model}"
+        return longName.title()
+    
+    def readOdometer(self):
+        print(f"This car has {self.odometerReading} miles on it")
+
+    def updateOdometer(self, milegae):
+        if milegae >= self.odometerReading:
+            self.odometerReading = milegae
+        else:
+            print("You can't roll back an odometer!")
+    
+    def incrementOdometer(self, miles):
+        self.odometerReading += miles
+
+    def fillGastank(self, addGas):
+        self.gasTank += addGas
+
+class ElectricCar(Car):
+    def __init__(self, make,model,year, large_battery=Battery()):
+        super().__init__(make,model,year)
+        self.battery = large_battery
+
+    def fillGastank(self):
+        print("This car doesn't have a gas tank!")
+    def describe_bettery(self):
+        self.battery = Battery()
+
+class Battery:
+    def __init__(self,battery_size=00):
+        self.battery_size = battery_size
+
+    def decribeBattry(self):
+        print(f"This car has a {self.batterySize}-kWh battery.")
+
+    def getRange(self):
+        if self.batterySize == 40:
+            range = 150
+        elif self.batterySize == 65:
+            range = 225
+
+        print(f"This car can go about {range} miles on a full charge.")
+
+myLeaf = ElectricCar('nissan', 'leaf', '2024')
+print(myLeaf.getDescriptiveName())
+myLeaf.battery.decribeBattry()
+myLeaf.fillGastank()
+myLeaf.battery.getRange()
+
+large_battery = Battery(80)
+myLeaf_large_battery = ElectricCar('nisson', 'leaf', '2023', large_battery)
+myLeaf_large_battery.getDescriptiveName()
+
+class Car:
     """자동차 클래스"""
     def __init__(self, make, model, year):
         self.make=make
