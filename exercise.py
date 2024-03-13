93235b623cf350f1f7d3467e304069f9981e632a
Kim Engse <error302@naver.com>
2024-03-13 15:37:54 +0900

    24.03.13 v2

--- exercise.py
+++ exercise.py
@@ -1,13 +1,7 @@
 import sys
-n = int(sys.stdin.readline())
-x, y  = map(int, sys.stdin.readline().split())
-
-max_x, min_x = x, x
-max_y, min_y = y, y
-for _ in range(n-1) :
-    x, y  = map(int, sys.stdin.readline().split())
-    max_x = max(x, max_x)
-    max_y = max(y, max_y)
-    min_x = min(x, min_x)
-    min_y = min(y, min_y)
-print((max_x-min_x)*(max_y-min_y))
+line = list(map(int, sys.stdin.readline().split()))
+line.sort()
+if line[2] >= line[1] + line[0]:
+    print(line[1] + line[0] + line[1] + line[0] - 1)
+else :
+    print(sum(line))
