cd54d4014644265945c38ec16a088402892e2d3e
Kim Engse <error302@naver.com>
2024-03-15 09:18:39 +0900

    24.03.15 v1

--- exercise.py
+++ exercise.py
@@ -1,7 +1,11 @@
 import sys
-line = list(map(int, sys.stdin.readline().split()))
-line.sort()
-if line[2] >= line[1] + line[0]:
-    print(line[1] + line[0] + line[1] + line[0] - 1)
-else :
-    print(sum(line))
+n, score = map(int, input().split())
+num = list(map(int, sys.stdin.readline().split()))
+count = list()
+
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+             count.append(num[i] + num[j] + num[k])
+if(score in count) : print(score)
+
