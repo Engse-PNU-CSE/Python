eb73e2965822dfe6792d59d2421bdbdb32af01a6
Kim Engse <error302@naver.com>
2024-03-20 11:55:04 +0900

    24.03.20 v2

--- 16/prac.py
+++ 16/prac.py
@@ -1,8 +1,9 @@
 import json
 
 # 1. json - dump, load <--- file
-# 2. json - dumps, loads <--- 
+# 2. json - dumps, loads <--- str -> dict, dict -> str
 
-data = {"name":"kim"}
-
-print(json.dumps(data))
+with open('./16/mapping_global_datasets/eq_data/eq_data_1_day_m1.geojson', 'r', encoding='utf-8') as f:
+    data = json.load(f)
+    print(type(data), type(data['metadata']), type(data['metadata']['count']))
+    print(data['type'][0]['geometry']['coordinates'])
