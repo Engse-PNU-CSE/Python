995b8f4cc7af9e1f3291b844859e4195943e448a
Kim Engse <error302@naver.com>
2024-03-20 12:23:49 +0900

    24.03.20 v3

--- 16/prac.py
+++ 16/prac.py
@@ -1,9 +1,18 @@
 import json
-
+import plotly.express as px
 # 1. json - dump, load <--- file
 # 2. json - dumps, loads <--- str -> dict, dict -> str
 
+mag = []
+lat = []
+lon = []
+
 with open('./16/mapping_global_datasets/eq_data/eq_data_1_day_m1.geojson', 'r', encoding='utf-8') as f:
     data = json.load(f)
-    print(type(data), type(data['metadata']), type(data['metadata']['count']))
-    print(data['type'][0]['geometry']['coordinates'])
+    for feature in data['features']:
+        mag.append(feature['properties']['mag'])
+        lon.append(feature['geometry']['coordinates'][0])
+        lat.append(feature['geometry']['coordinates'][1])
+
+fig = px.scatter_geo(lat=lat, lon=lon, title="Wow")
+fig.show()
